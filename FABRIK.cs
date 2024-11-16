d7ffd5596d2e63a538558d6b9a08be2052c73314
CALVlN <36027062+CALVlN@users.noreply.github.com>
2024-11-15 20:39:25 -0800

    Created FABRIK script

---
+++ FABRIK.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FABRIK : MonoBehaviour
+{
+    // Variables
+    public int numJoints = 3;
+
+    // Start is called before the first frame update
+    void Start() {
+        Transform[] jointPositions = new Transform[numJoints];
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
